2da2e6229383b05b0fe8891580efd1c006c87a20
ianreay <ian.reay@gmail.com>
2013-12-02 05:59:10 -0700

    updates dot files to dos2unix them as well as make switching between windows and unix easy

---
+++ copy.sh
@@ -0,0 +1,6 @@
+cp _bashrc ~/.bashrc
+cp _vimrc ~/.vimrc
+cp unix.vim ~/unix.vim
+cp cscope.vim ~/cscope.vim
+cp bundles.vim ~/bundles.vim
+cp _gvimrc ~/.gvimrc
---
+++ unix.vim
@@ -0,0 +1,2 @@
+let g:vimwiki_list = [{'path': '~/Dropbox/vimwiki'}]
+
